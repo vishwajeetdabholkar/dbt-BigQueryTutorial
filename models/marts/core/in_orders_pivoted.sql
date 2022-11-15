805a2793786c55262b07011233976cd1c9c8bb37
Vishwajeet Dabholkar <vishwajeet.dabholkar@freshgravity.com>
2022-11-15 11:04:31 +0000

    added jinja code to remove hardcoded values , added formatting using jinja for more readable complied query

--- models/marts/core/in_orders_pivoted.sql
+++ models/marts/core/in_orders_pivoted.sql
@@ -1,19 +1,17 @@
+{%- set payment_methods = ['bank_transfer','credit_card','coupon','gift_card'] -%}
 with payments as (
-
     select * from {{ ref('stg_payments')}}
 ),
 
 pivoted as (
     select 
      order_id,
-     {% set payment_methods = ['bank_transfer','credit_card','coupon','gift_card'] %}
-     {% for payment_method in payment_methods %}
-
-     {% if not loop.last%}
+     {%- for payment_method in payment_methods -%}
+     {%- if not loop.last -%}
      sum(case when payment_method = {{ payment_method }} then amount else 0 end ) as {{payment_method}}_amount,
-     {% endif %}
+     {%- endif -%}
      sum(case when payment_method = {{ payment_method }} then amount else 0 end ) as {{payment_method}}_amount 
-     {% endfor % }
+     {%- endfor -% }
      from payments
      where status = 'success'
      group by order_id
